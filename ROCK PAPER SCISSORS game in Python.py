a26b1c563dde38e6c755633dbe361727b5afdbab
AbrarMojahidRafi <rafi.cse.bracu@gmail.com>
2023-07-03 19:05:21 +0600

    some update happend

--- ROCK PAPER SCISSORS game in Python.py
+++ ROCK PAPER SCISSORS game in Python.py
@@ -1,1 +1,10 @@
-print("Hello World")
+import random
+
+options = ("rock", "paper", "scissors")
+computer = random.choice(options)
+
+you = input("Enter a choice (rock/paper/scissors): ")
+if you not in options: 
+    print('Please enter a valid choice.')
+
+print(computer, you)

e1c2a0819d111a97bd16ed13c4ad6b09212462ca
AbrarMojahidRafi <rafi.cse.bracu@gmail.com>
2023-07-25 00:13:43 +0600

    completed

--- ROCK PAPER SCISSORS game in Python.py
+++ ROCK PAPER SCISSORS game in Python.py
@@ -1,10 +1,20 @@
 import random
 
 options = ("rock", "paper", "scissors")
-computer = random.choice(options)
+generateComputerChoice = random.choice(options)
 
-you = input("Enter a choice (rock/paper/scissors): ")
-if you not in options: 
+yourChoice = input("Enter a choice (rock/paper/scissors): ").lower()
+if yourChoice not in options:
     print('Please enter a valid choice.')
+else: 
+  print(f"COMPUTER CHOICE: {generateComputerChoice}")
+  print(f"YOU CHOICE: {yourChoice}")
+  print("="*22)
+  if generateComputerChoice == yourChoice:
+    print("Game TIE")
+  else:
+    if (generateComputerChoice == "rock" and yourChoice == "scissors") or (generateComputerChoice == "paper" and yourChoice == "rock") or (generateComputerChoice == "scissors" and yourChoice == "paper"):
+      print("COMPUTER wins the game!")
+    else:
+      print("YOU wins the game!")
 
-print(computer, you)
